50d9eefdcfb0427b7fa2c60c867252858bd2e5d3
JonConStudio <hello@jonconstantine.co.uk>
2025-12-30 23:16:51 +0000

    changed the append to txt file to pipe command to verify the out put on the text file is corret. Running into an issue on History command

--- q22.sh
+++ q22.sh
@@ -1,56 +1,56 @@
 HISTFILE=~/.bash_history
 set -o history
-echo ======================================= 	> JonathanConstantine.txt #over-writes or creates the text file. 
-echo    Jonathan Constantine CATC Project    	>> JonathanConstantine.txt 
-echo ======================================= 	>> JonathanConstantine.txt #creating a header to differentiate the output  
-echo                                         	>> JonathanConstantine.txt #SPACE
-echo Date & date                               	>> JonathanConstantine.txt #Date Header with date appended                        
-echo                                         	>> JonathanConstantine.txt #SPACE
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Host Name 					>> JonathanConstantine.txt 
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo                                            >> JonathanConstantine.txt
-hostname 					>> JonathanConstantine.txt #hostname command
-echo 						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Server Architecture                        >> JonathanConstantine.txt #HEADER
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo                                            >> JonathanConstantine.txt
-arch						>> JonathanConstantine.txt #list the architecture
-echo 						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Users 	                                >> JonathanConstantine.txt #HEADER 
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo                                            >> JonathanConstantine.txt
-uname -a 					>> JonathanConstantine.txt #lists all users on the server
-echo 						>> JonathanConstantine.txt #SPACE
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Server Uptime                              >> JonathanConstantine.txt #HEADER
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo 						>> JonathanConstantine.txt 
-uptime 						>> JonathanConstantine.txt
-echo 						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo My User Details                            >> JonathanConstantine.txt 
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-whoami 						>> JonathanConstantine.txt
-echo 						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Who is online                              >> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo                                            >> JonathanConstantine.txt
-who 						>> JonathanConstantine.txt
-echo 						>> JonathanConstantine.txt
-w 						>> JonathanConstantine.txt
-echo						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo System Resources Overview                  >> JonathanConstantine.txt 
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo					        >> JonathanConstantine.txt
-top -b -n 1 					>> JonathanConstantine.txt
-echo 						>> JonathanConstantine.txt
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
-echo Command History				>> JonathanConstantine.txt #DIVIDER
-echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo ======================================= 	| tee JonathanConstantine.txt #over-writes or creates the text file. 
+echo    Jonathan Constantine CATC Project    	| tee -a JonathanConstantine.txt #A title for the report
+echo ======================================= 	| tee -a JonathanConstantine.txt #creating a header to differentiate the output  
+echo                                         	| tee -a JonathanConstantine.txt #SPACE
+echo Date & date                               	| tee -a JonathanConstantine.txt #Date Header with date appended                        
+echo                                            | tee -a JonathanConstantine.txt #SPACE
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Host Name 					| tee -a JonathanConstantine.txt 
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo                                            | tee -a JonathanConstantine.txt
+hostname 					| tee -a JonathanConstantine.txt #hostname command
+echo 						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Server Architecture                        | tee -a JonathanConstantine.txt #HEADER
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo                                            | tee -a JonathanConstantine.txt
+arch						| tee -a JonathanConstantine.txt #list the architecture
+echo 						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Users 	                                | tee -a JonathanConstantine.txt #HEADER 
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo                                            | tee -a JonathanConstantine.txt
+uname -a 					| tee -a JonathanConstantine.txt #lists all users on the server
+echo 						| tee -a JonathanConstantine.txt #SPACE
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Server Uptime                              | tee -a JonathanConstantine.txt #HEADER
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo 						| tee -a JonathanConstantine.txt 
+uptime 						| tee -a JonathanConstantine.txt #prints server details
+echo 						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo My User Details                            | tee -a JonathanConstantine.txt 
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+whoami 						| tee -a JonathanConstantine.txt #lists my logged in user details
+echo 						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Who is online                              | tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo                                            | tee -a JonathanConstantine.txt
+who 						| tee -a JonathanConstantine.txt #who commannd (who  is online)
+echo 						| tee -a JonathanConstantine.txt
+w 						| tee -a JonathanConstantine.txt #w command, more detailed, tabular view
+echo						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo System Resources Overview                  | tee -a JonathanConstantine.txt 
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo					        | tee -a JonathanConstantine.txt
+top -b -n 1 					| tee -a JonathanConstantine.txt #prints system resources
+echo 						| tee -a JonathanConstantine.txt
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
+echo Command History				| tee -a JonathanConstantine.txt #DIVIDER
+echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
 echo >> JonathanConstantine.txt
 history >> JonathanConstantine.txt
