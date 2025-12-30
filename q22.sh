195b18185b31cc14f4257986e5afe47b998525c7
JonConStudio <hello@jonconstantine.co.uk>
2025-12-30 22:55:29 +0000

    updated q22.sh code resulted in: error line 2: ry: command not found, tried to be too fancy

--- q22.sh
+++ q22.sh
@@ -1,24 +1,57 @@
-HISTFILE=~/.bash_history
+HISTFILE=~/.bash_histo
+ry
 set -o history
-echo Jonathan Constantine CATC Project > JonathanConstantine.txt #writes or creates the text file. 
-echo >> JonathanConstantine.txt
-date >> JonathanConstantine.txt #appends the date to text file
-echo >> JonathanConstantine.txt
-hostname >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-arch >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-uname -a >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-uptime >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-whoami >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-who >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-w >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
-top -b -n 1 >> JonathanConstantine.txt
-echo >> JonathanConstantine.txt
+echo ======================================= 	> JonathanConstantine.txt #over-writes or creates the text file. 
+echo    Jonathan Constantine CATC Project    	>> JonathanConstantine.txt 
+echo ======================================= 	>> JonathanConstantine.txt #creating a header to differentiate the output  
+echo                                         	>> JonathanConstantine.txt #SPACE
+echo Date                                    	>> JonathanConstantine.txt #Date Header 
+date                                         	>> JonathanConstantine.txt #appends the date to text file
+echo                                         	>> JonathanConstantine.txt #SPACE
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Host Name 					>> JonathanConstantine.txt 
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo                                            >> JonathanConstantine.txt
+hostname 					>> JonathanConstantine.txt #hostname command
+echo 						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Server Architecture                        >> JonathanConstantine.txt #HEADER
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo                                            >> JonathanConstantine.txt
+arch						>> JonathanConstantine.txt #list the architecture
+echo 						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Users 	                                >> JonathanConstantine.txt #HEADER 
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo                                            >> JonathanConstantine.txt
+uname -a 					>> JonathanConstantine.txt #lists all users on the server
+echo 						>> JonathanConstantine.txt #SPACE
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Server Uptime                              >> JonathanConstantine.txt #HEADER
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+uptime 						>> JonathanConstantine.txt
+echo 						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo My User Details                            >> JonathanConstantine.txt 
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+whoami 						>> JonathanConstantine.txt
+echo 						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Who is online                              >> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo                                            >> JonathanConstantine.txt
+who 						>> JonathanConstantine.txt
+echo 						>> JonathanConstantine.txt
+w 						>> JonathanConstantine.txt
+echo						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo System Resources Overview                  >> JonathanConstantine.txt 
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo					        >> JonathanConstantine.txt
+top -b -n 1 					>> JonathanConstantine.txt
+echo 						>> JonathanConstantine.txt
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
+echo Command History				>> JonathanConstantine.txt #DIVIDER
+echo =======================================    >> JonathanConstantine.txt #DIVIDER
 echo >> JonathanConstantine.txt
 history >> JonathanConstantine.txt
