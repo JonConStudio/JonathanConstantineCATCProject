0640f1c3d0d5d211a4579755beb8ceaa98ee2c20
JonConStudio <hello@jonconstantine.co.uk>
2025-12-30 23:36:44 +0000

    realised the text file was setup incorrectly according to assignment requirements

--- q22.sh
+++ q22.sh
@@ -1,56 +1,56 @@
 HISTFILE=~/.bash_history
 set -o history
-echo ======================================= 	| tee JonathanConstantine.txt #over-writes or creates the text file. 
-echo    Jonathan Constantine CATC Project    	| tee -a JonathanConstantine.txt #A title for the report
-echo ======================================= 	| tee -a JonathanConstantine.txt #creating a header to differentiate the output  
-echo                                         	| tee -a JonathanConstantine.txt #SPACE
-echo Date & date                               	| tee -a JonathanConstantine.txt #Date Header with date appended                        
-echo                                            | tee -a JonathanConstantine.txt #SPACE
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Host Name 					| tee -a JonathanConstantine.txt 
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo                                            | tee -a JonathanConstantine.txt
-hostname 					| tee -a JonathanConstantine.txt #hostname command
-echo 						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Server Architecture                        | tee -a JonathanConstantine.txt #HEADER
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo                                            | tee -a JonathanConstantine.txt
-arch						| tee -a JonathanConstantine.txt #list the architecture
-echo 						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Server Information				| tee -a JonathanConstantine.txt #HEADER 
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo                                            | tee -a JonathanConstantine.txt
-uname -a 					| tee -a JonathanConstantine.txt #lists the server information  in detail 
-echo 						| tee -a JonathanConstantine.txt #SPACE
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Server Uptime                              | tee -a JonathanConstantine.txt #HEADER
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo 						| tee -a JonathanConstantine.txt 
-uptime 						| tee -a JonathanConstantine.txt #prints server details
-echo 						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo My User Details                            | tee -a JonathanConstantine.txt 
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-whoami 						| tee -a JonathanConstantine.txt #lists my logged in user details
-echo 						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Who is online                              | tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo                                            | tee -a JonathanConstantine.txt
-who 						| tee -a JonathanConstantine.txt #who commannd (who  is online)
-echo 						| tee -a JonathanConstantine.txt
-w 						| tee -a JonathanConstantine.txt #w command, more detailed, tabular view
-echo						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo System Resources Overview                  | tee -a JonathanConstantine.txt 
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo					        | tee -a JonathanConstantine.txt
-top -b -n 1 					| tee -a JonathanConstantine.txt #prints system resources
-echo 						| tee -a JonathanConstantine.txt
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo Command History				| tee -a JonathanConstantine.txt #DIVIDER
-echo =======================================    | tee -a JonathanConstantine.txt #DIVIDER
-echo 						| tee -a JonathanConstantine.txt
-history 					| tee -a JonathanConstantine.txt #lists all commands made by this user
+echo ======================================= 	| tee jonathanConstantine.txt #over-writes or creates the text file. 
+echo    Jonathan Constantine CATC Project    	| tee -a jonathanConstantine.txt #A title for the report
+echo ======================================= 	| tee -a jonathanConstantine.txt #creating a header to differentiate the output  
+echo                                         	| tee -a jonathanConstantine.txt #SPACE
+echo Date & date                               	| tee -a jonathanConstantine.txt #Date Header with date appended                        
+echo                                            | tee -a jonathanConstantine.txt #SPACE
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Host Name 					| tee -a jonathanConstantine.txt 
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo                                            | tee -a jonathanConstantine.txt
+hostname 					| tee -a jonathanConstantine.txt #hostname command
+echo 						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Server Architecture                        | tee -a jonathanConstantine.txt #HEADER
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo                                            | tee -a jonathanConstantine.txt
+arch						| tee -a jonathanConstantine.txt #list the architecture
+echo 						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Server Information				| tee -a jonathanConstantine.txt #HEADER 
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo                                            | tee -a jonathanConstantine.txt
+uname -a 					| tee -a jonathanConstantine.txt #lists the server information  in detail 
+echo 						| tee -a jonathanConstantine.txt #SPACE
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Server Uptime                              | tee -a jonathanConstantine.txt #HEADER
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo 						| tee -a jonathanConstantine.txt 
+uptime 						| tee -a jonathanConstantine.txt #prints server details
+echo 						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo My User Details                            | tee -a jonathanConstantine.txt 
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+whoami 						| tee -a jonathanConstantine.txt #lists my logged in user details
+echo 						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Who is online                              | tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo                                            | tee -a jonathanConstantine.txt
+who 						| tee -a jonathanConstantine.txt #who commannd (who  is online)
+echo 						| tee -a jonathanConstantine.txt
+w 						| tee -a jonathanConstantine.txt #w command, more detailed, tabular view
+echo						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo System Resources Overview                  | tee -a jonathanConstantine.txt 
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo					        | tee -a jonathanConstantine.txt
+top -b -n 1 					| tee -a jonathanConstantine.txt #prints system resources
+echo 						| tee -a jonathanConstantine.txt
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo Command History				| tee -a jonathanConstantine.txt #DIVIDER
+echo =======================================    | tee -a jonathanConstantine.txt #DIVIDER
+echo 						| tee -a jonathanConstantine.txt
+history 					| tee -a jonathanConstantine.txt #lists all commands made by this user
