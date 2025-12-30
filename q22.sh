a81ac77b96b59ca042db6eb5f54751bce799c3a2
JonConStudio <hello@jonconstantine.co.uk>
2025-12-30 22:24:45 +0000

    created the first pass of having the shell script output to the text file

--- q22.sh
+++ q22.sh
@@ -1,22 +1,24 @@
 HISTFILE=~/.bash_history
 set -o history
-echo Jonathan Constantine CATC Project
-echo 
-date
-echo
-hostname
-echo
-arch
-echo
-uname -a
-echo
-uptime
-echo
-whoami
-echo
-who
-echo
-w
-echo
-top -b -n 1
-history
+echo Jonathan Constantine CATC Project > JonathanConstantine.txt #writes or creates the text file. 
+echo >> JonathanConstantine.txt
+date >> JonathanConstantine.txt #appends the date to text file
+echo >> JonathanConstantine.txt
+hostname >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+arch >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+uname -a >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+uptime >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+whoami >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+who >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+w >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+top -b -n 1 >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+echo >> JonathanConstantine.txt
+history >> JonathanConstantine.txt
